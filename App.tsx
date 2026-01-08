bad09a7d6ab93d1deb2467c282b8f2e015d32b48
sakalesha <ronadasakalesha@gmail.com>
2026-01-08 20:56:09 +0530

    Fix Vercel deployment: switch to BrowserRouter and improve env var handling

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import DashboardPage from './pages/DashboardPage';
 import EntriesPage from './pages/EntriesPage';
 import EntryDetailPage from './pages/EntryDetailPage';
 import NewEntryPage from './pages/NewEntryPage';
 import PortfolioPage from './pages/PortfolioPage';
-
 const App: React.FC = () => {
   return (
     <Router>
